ede2b8844bf3fd13945c0a839f870e7bc996810e
Yevgeny Levin <devlev1980@gmail.com>
2017-12-22 23:17:33 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+var srcValue = $('.imgHolder img').attr('src');
+
+// alert(srcValue);
+
+$('.colorBlue').on('click',function(){
+  $('.imgHolder img').attr('src','img/mini-blue.jpg')
+});
+
+$('.colorRed').on('click',function(){
+  $('.imgHolder img').attr('src','img/mini-red.jpg')
+})
+$('.colorBlack').on('click',function(){
+  $('.imgHolder img').attr('src','img/mini-black.jpg')
+})
+$('.colorPurple').on('click',function(){
+  $('.imgHolder img').attr('src','img/mini-purple.jpg')
+})
+$('.colorGreen').on('click',function(){
+  $('.imgHolder img').attr('src','img/mini-green.jpg')
+})
+$('.colorGraphite').on('click',function(){
+  $('.imgHolder img').attr('src','img/mini-graphite.jpg')
+})
+$('.colorDarkYellow').on('click',function(){
+  $('.imgHolder img').attr('src','img/mini-dark-yellow.jpg')
+})
